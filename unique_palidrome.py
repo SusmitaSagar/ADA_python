c6f16fd5435999d631beca50ef694b02bae9438b
SusmitaSagar_202124 <susmitasagar038@gmail.com>
2022-08-19 19:32:38 +0530

    Update unique_palidrome.py

--- unique_palidrome.py
+++ unique_palidrome.py
@@ -1,28 +1,25 @@
-
-def find_palindromes_in_sub_string(input, j, k):
-  #count=0
-  while j >= 0 and k < len(input):
-    if input[j] != input[k]:
-      break
-    else:
-        print(input[j: k + 1])
-    #count += 1
-
-    j -= 1
-    k += 1
-
-  return 
+def expand(s, left, right):
+    len_s = len(s)
+    while left >= 0 and right < len_s and s[left] == s[right]:
+        left -= 1
+        right += 1
+    return left+1, right-1
 
 
-"""def find_all_palindrome_substrings(input):
-  count = 0
-  for i in range(0, len(input)):
-    count += find_palindromes_in_sub_string(input, i - 1, i + 1)
-    count += find_palindromes_in_sub_string(input, i, i + 1)"""
+def get_unique_palindromes(s):
+    len_s = len(s)
+    result = []
+    for i in range(len_s):
+        l1, r1 = expand(s, i, i)
+        l2, r2 = expand(s, i, i+1)
 
-  #return count
+        if l1 != r1:
+            result.append(s[l1:r1+1])
+        if l2 < r2:
+            result.append(s[l2:r2+1])
+    return result
 
 
-s = "aabbbaa"
-print("all palidrome",find_palindromes_in_sub_string(s,10,10))
-#print("Total palindrome substrings: ", find_all_palindrome_substrings(s))
+if __name__ == '__main__':
+    string = input("Enter a Word: ")
+    print(f"All unique palindromes are :{get_unique_palindromes(string)}")
