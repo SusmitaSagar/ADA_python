efdf06afc71c686a50d1f61f0d692b424f380e84
SusmitaSagar_202124 <susmitasagar038@gmail.com>
2022-08-19 23:15:28 +0530

    Update unique_palidrome.py

--- unique_palidrome.py
+++ unique_palidrome.py
@@ -1,25 +1,34 @@
+# to find all unique palindrome in string
+# function To expand left and right pointers to find the longest palindrome
 def expand(s, left, right):
     len_s = len(s)
-    while left >= 0 and right < len_s and s[left] == s[right]:
+    while left >= 0 and right < len_s and s[left] == s[right]: ####
         left -= 1
         right += 1
     return left+1, right-1
 
 
+# function to find all unique palindromes in string
 def get_unique_palindromes(s):
     len_s = len(s)
-    result = []
+    result = set()  # using set, so that only unique palindromes will be returned
     for i in range(len_s):
         l1, r1 = expand(s, i, i)
         l2, r2 = expand(s, i, i+1)
 
+        # condition to avoid single letters in odd palindrome case
         if l1 != r1:
-            result.append(s[l1:r1+1])
+            result.add(s[l1:r1+1])
+
+        # condition to check for even palindrome case
         if l2 < r2:
-            result.append(s[l2:r2+1])
-    return result
+            result.add(s[l2:r2+1])
+
+        # returns set as a list so that no two are same
+    return list(result)
 
 
+# run this code if this file is executed directley in the terminal
 if __name__ == '__main__':
     string = input("Enter a Word: ")
     print(f"All unique palindromes are :{get_unique_palindromes(string)}")
